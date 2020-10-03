09f79056f02cca224e3fd2371402342a32871cd9
Justin Eldridge (bot) <jeldridge@ucsd.edu>
2020-10-02 18:21:00 -0700

    autocommit

---
+++ labs/01-python_basics/tests/q33.py
@@ -0,0 +1,10 @@
+test = {   'name': 'q33',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> seconds_in_an_hour != 60 # oops! the number of seconds in an hour is not quite that. it is somewhere between 59 and 61...\nFalse',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {'code': '>>> seconds_in_an_hour == 60\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
